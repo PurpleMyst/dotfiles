d6121b0b99086bda92506a056b7f0f31331d5b78
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-06-23 13:54:55 +0200

    fix: include yarn + nvim in nvm shims

--- .oh-my-zsh/custom/nvm.zsh
+++ .oh-my-zsh/custom/nvm.zsh
@@ -9,7 +9,15 @@ if [ -d "$NVM_DIR" -a -f "$NVM_DIR/nvm.sh" ]; then
 
     __create_nvm_shim() {
         __NVM_SHIMS+=("$1")
-        eval "$1() { __load_nvm; $1 \"\$@\" }"
+        eval "$1() { __load_nvm; \"$1\" \"\$@\" }"
+    }
+
+    __create_nvm_shims() {
+        if [ -d "$1" ]; then
+            find "$1" -maxdepth 3 -type f,l -path '*/bin/*' -exec basename {} \; \
+            | sort -u \
+            | while read -r name; do __create_nvm_shim "$name"; done
+        fi
     }
 
     __create_nvm_shim nvm
@@ -17,9 +25,9 @@ if [ -d "$NVM_DIR" -a -f "$NVM_DIR/nvm.sh" ]; then
     # for coc.nvim
     __create_nvm_shim nvim
 
-    if [ -d "$NVM_DIR/versions/node" ]; then
-        find "$NVM_DIR/versions/node/" -maxdepth 3 -type f,l -path '*/bin/*' -exec basename {} \; \
-        | sort -u \
-        | while read -r name; do __create_nvm_shim "$name"; done
-    fi
+    test -x "$(command -v phanes)" && __create_nvm_shim phanes
+
+    __create_nvm_shims "$NVM_DIR/versions/node"
+
+    __create_nvm_shims "$HOME/.yarn/bin/"
 fi
