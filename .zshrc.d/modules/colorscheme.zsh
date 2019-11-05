bc4cdf792041b0eea9e25587dc97ba7f8c45032e
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-11-05 23:42:50 +0100

    zsh: create base16 color associative array

--- .zshrc.d/modules/colorscheme.zsh
+++ .zshrc.d/modules/colorscheme.zsh
@@ -5,3 +5,7 @@ BASE16_COLORSCHEME=$(cat ~/.colorscheme)
 export BASE16_COLORSCHEME
 
 [ -f "$BASE16_OUTPUT_DIR/shell/scripts/base16-$BASE16_COLORSCHEME.sh" ] && sh "$BASE16_OUTPUT_DIR/shell/scripts/base16-$BASE16_COLORSCHEME.sh"
+
+# Declare an associative array whose keys are the two-digit hex code for each
+# base16 color, and whose values are the hex colors
+typeset -A BASE16_HEX_COLORS=($(rg 'base(\d+)\[\] = "([^"]+)"' -or '$1 $2' "$BASE16_OUTPUT_DIR/c_header/headers/base16-$BASE16_COLORSCHEME.h"))
