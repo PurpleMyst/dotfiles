dac58255963e308729adbc7f56c54fdc0d32cb3a
Purple Myst <PurpleMyst@users.noreply.github.com>
2020-02-25 12:54:37 +0100

    refactor: modernize set_colorscheme

--- .zshrc.d/modules/colorscheme.zsh
+++ .zshrc.d/modules/colorscheme.zsh
@@ -1,14 +1,14 @@
-BASE16_BUILDER_REPO="https://github.com/InspectorMustache/base16-builder-python.git"
-BASE16_OUTPUT_DIR="$HOME/applications/base16-builder/output"
+export BASE16_BUILDER_REPO='https://github.com/InspectorMustache/base16-builder-python.git'
+export BASE16_OUTPUT_DIR="$HOME/applications/base16-builder/output"
 
-BASE16_COLORSCHEME=$(cat ~/.colorscheme)
-export BASE16_COLORSCHEME
+if [[ -f ~/.colorscheme ]] export BASE16_COLORSCHEME=$(<~/.colorscheme)
 
-[ -f "$BASE16_OUTPUT_DIR/shell/scripts/base16-$BASE16_COLORSCHEME.sh" ] && sh "$BASE16_OUTPUT_DIR/shell/scripts/base16-$BASE16_COLORSCHEME.sh"
+SHELL_SCRIPT="$BASE16_OUTPUT_DIR/shell/scripts/base16-$BASE16_COLORSCHEME.sh"
+if [[ -f $SHELL_SCRIPT ]] sh $SHELL_SCRIPT
+unset SHELL_SCRIPT
 
 # Declare an associative array whose keys are the two-digit hex code for each
 # base16 color, and whose values are the hex colors
-
 if [[ ${commands[rg]} ]]; then
     typeset -A BASE16_HEX_COLORS=($(rg 'base([0-9A-F]+)\[\] = "([^"]+)"' -or '$1 $2' "$BASE16_OUTPUT_DIR/c_header/headers/base16-$BASE16_COLORSCHEME.h"))
 fi
